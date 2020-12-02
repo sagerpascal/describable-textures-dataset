0716d959459708d3bc15010c1e78eae15ba9dd82
Pascal Sager <sage@zhaw.ch>
2020-12-02 12:59:24 +0100

    Cleanup code

--- dataset.py
+++ dataset.py
@@ -5,9 +5,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from PIL import Image
+import yaml
 
+# Read Config
+conf = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+num_classes = conf['num_classes']
 
-# Mask range: 0-47
+# Mask range: 0-46
 class DtdDataset(torch.utils.data.Dataset):
 
     def __init__(self, image_path, mask_size, augmentation=None, preprocessing=None):
@@ -35,7 +39,7 @@ class DtdDataset(torch.utils.data.Dataset):
     def __getitem__(self, item):
 
         filename = self.files[item]
-        img = Image.open(os.path.join(self.image_path, self.image_prefix + filename)).convert("L")
+        img = Image.open(os.path.join(self.image_path, self.image_prefix + filename)).convert("RGB")
         mask = Image.open(os.path.join(self.image_path, self.mask_prefix + filename))
         img = np.array(img)
         mask = np.array(mask)
@@ -44,17 +48,14 @@ class DtdDataset(torch.utils.data.Dataset):
         mask = self.resize_mask(mask)
         img = self.normalize_image(img)
 
-
         # apply augmentation
         if self.augmentation is not None:
             sample = self.augmentation(image=img)
             img = sample['image']  # TODO: Augment mask also
 
-
         label = np.array(mask[0][0])
         # check if all labels are equal
         assert np.all(mask == label)
-
 
         # apply preprocessing
         if self.preprocessing is not None:
@@ -62,7 +63,6 @@ class DtdDataset(torch.utils.data.Dataset):
             img, mask = sample['image'], sample['mask']
 
         return img, mask, label
-
 
     def resize_mask(self, mask):
         y, x = mask.shape
@@ -130,19 +130,48 @@ def get_validation_augmentation():
     return A.Compose(valid_transform)
 
 
+def to_tensor(x, **kwargs):
+    '''
+    Convert image to tensor
+    '''
+    return torch.as_tensor(x.transpose(2, 0, 1).astype('float32'))
+
+
+def to_tensor_mask(x, **kwargs):
+    '''
+    Convert mask to tensor
+    '''
+    # With BCELossWithDigits
+    x_tensor = torch.as_tensor(x.astype('int64'))
+    x_oh = torch.nn.functional.one_hot(x_tensor, num_classes=num_classes).type(torch.DoubleTensor)
+    return x_oh.permute(2, 0, 1)
+    # With CrossEntropyLoss
+    # return torch.as_tensor(x.astype('int64'))
+
+
+def get_preprocessing():
+    '''
+    Preprocess the image and the mask
+    '''
+    _transform = [
+        A.Lambda(image=to_tensor, mask=to_tensor_mask),
+    ]
+    return A.Compose(_transform)
+
+
 def plot_some_data():
-    dataset = DtdDataset('images/dtd_train')
+    def unormalize_image(img):
+        return (img - np.min(img)) / (np.max(img) - np.min(img))
+
+    dataset = DtdDataset('data/dtd_train_tiled', mask_size=(128, 128), preprocessing=get_preprocessing())
 
     for i in range(10):
-        img, lab = dataset[i]
-        plt.imshow(img)
+        img, _, lab = dataset[i]
+        img = img.numpy().transpose(1, 2, 0)
+        plt.imshow(unormalize_image(img))
         plt.title("Label: {}".format(lab))
         plt.show()
 
 
 if __name__ == '__main__':
     plot_some_data()
-
-    dataset = DtdDataset('images/dtd_train')
-    for i in range(len(dataset)):
-        dataset[i]
--- train.py
+++ train.py
@@ -1,78 +1,67 @@
 import argparse
-
 import albumentations as A
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-
-from dataset import DtdDataset, get_training_augmentation, get_validation_augmentation
+from dataset import DtdDataset, get_training_augmentation, get_validation_augmentation, get_preprocessing
 from epoch import TrainEpoch, ValidEpoch
 from metrics import Accuracy, Fscore, Recall, Precision, AccuracyT1
 from models import SimpleFullyCnn, SimpleUNet
+import yaml
 
-# Config
-device = 'cpu'
-num_classes = 47
-max_num_epoch = 75
+# Read Config
+conf = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+num_classes = conf['num_classes']
+device = conf['device']
+max_num_epoch = conf['max_num_epoch']
 
-# Parameters from command line
+# Read parameters from command line
 parser = argparse.ArgumentParser()
 parser.add_argument("--learning_rate")
 parser.add_argument("--batch_size")
-parser.add_argument("--pweight_factor")
 parser.add_argument("--model_name")
 parser.add_argument("--wandb")
 
 args = parser.parse_args()
 lr = float(args.learning_rate)  # 0.000005
 batch_size = int(args.batch_size)  # 8
-pos_weight_factor = int(args.pweight_factor)
 model_name = str(args.model_name)
 use_wandb = bool(args.wandb)
 
 torch.cuda.empty_cache()
 
 
-def to_tensor(x, **kwargs):
-    x = np.expand_dims(x, axis=0)
-    return torch.as_tensor(x.astype('float32'))
-
-
-def to_tensor_mask(x, **kwargs):
-    # With BCELossWithDigits
-    x_tensor = torch.as_tensor(x.astype('int64'))
-    x_oh = torch.nn.functional.one_hot(x_tensor, num_classes=num_classes).type(torch.DoubleTensor)
-    return x_oh.permute(2, 0, 1)
-    # With CrossEntropyLoss
-    # return torch.as_tensor(x.astype('int64'))
-
-
-def get_preprocessing():
-    _transform = [
-        A.Lambda(image=to_tensor, mask=to_tensor_mask),
-    ]
-    return A.Compose(_transform)
-
-
 def get_data_loaders(mask_size):
+    '''
+    Returns the dataloader for training, validation and testing
+    '''
     train_dataset = DtdDataset('data/dtd_train_tiled', mask_size, augmentation=get_training_augmentation(),
                                preprocessing=get_preprocessing())
     valid_dataset = DtdDataset('data/dtd_val_tiled', mask_size, augmentation=get_validation_augmentation(),
                                preprocessing=get_preprocessing())
-    test_dataset = DtdDataset('data/dtd_test_tiled', mask_size, augmentation=None, preprocessing=get_preprocessing())
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
-    return train_loader, valid_loader, test_loader
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2, multiprocessing_context=torch.multiprocessing.get_context('spawn'))
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=1, multiprocessing_context=torch.multiprocessing.get_context('spawn'))
+    return train_loader, valid_loader
 
 
 class MyWeightedLoss(torch.nn.BCEWithLogitsLoss):
+    '''
+    Weighted BCEWithLogitsLoss (weighted according mask distribution)
+    '''
+    __name__ = 'weighted-bce-with-logits-loss'
 
     def __init__(self, mask_size, **kwargs):
-        pos_weight = np.ones(
-            (num_classes, mask_size[0], mask_size[1])) * pos_weight_factor  # increase recall (for the cost of a worse precision)
+        pos_weight = np.ones((num_classes, mask_size[0], mask_size[1]))
+        weights = [0.956569204, 0.977921196, 1.408709632, 1.084427464, 1.315641729, 0.946239083, 0.952410208,
+                   0.905183255, 1.038172265, 0.82662018, 0.803869166, 0.995701581, 1.018857432, 1.114780396,
+                   0.905183255, 1.000248164, 0.944199775, 1.043115942, 0.954485176, 1.092540388, 0.946239083,
+                   0.938134252, 1.076434142, 0.825063457, 0.914631932, 0.842516722, 1.076434142, 1.040638232,
+                   1.014140499, 1.055683604, 0.973574879, 1.103548352, 1.269880277, 1.120482598, 0.954485176,
+                   0.876217391, 1.087118351, 0.847405601, 0.960764683, 1.505528164, 1.929994254, 0.857355569,
+                   0.895927803, 0.82662018, 0.988958681, 1.055683604]
+        for i, w in enumerate(weights):
+            pos_weight[i, :, :] = pos_weight[i, :, :] * w
         pos_weight = torch.FloatTensor(pos_weight)
         super().__init__(pos_weight=pos_weight, **kwargs)
 
@@ -80,31 +69,22 @@ class MyWeightedLoss(torch.nn.BCEWithLogitsLoss):
         return super(MyWeightedLoss, self).forward(y_pr, y_gt)
 
 
-def main():
-    import wandb
-    if use_wandb:
-        wandb.init(project="compvis_dtd_{}".format(model_name))
-        wandb.config.update({"Model": model_name,
-                             "Learning Rate": lr,
-                             "Batch Size": batch_size,
-                             "Pos- Weight": pos_weight_factor
-                             })
-    else:
-        from torch.utils.tensorboard import SummaryWriter
-        writer = SummaryWriter()
-
-    if model_name == 'Simple FCN':
+def get_model():
+    '''
+    Returns the model and the mask size
+    '''
+    if model_name == 'simple_fcn':
         model = SimpleFullyCnn(in_channels=3, out_channels=num_classes).to(device)
         mask_size = model.get_mask_size()
 
-    elif model_name == 'Simple U-Net':
+    elif model_name == 'simple_u-net':
         model = SimpleUNet(input_channels=3, output_channels=num_classes).to(device)
         mask_size = (128, 128)
 
-    elif model_name == 'U-Net pretrained encoder':
+    elif model_name == 'pretrained_u-net':
         import segmentation_models_pytorch as smp
         model = smp.Unet(
-            encoder_name='resnet101',
+            encoder_name='resnet50',
             encoder_weights='imagenet',
             classes=num_classes,
             in_channels=3,
@@ -112,14 +92,30 @@ def main():
             activation='softmax2d'
         )
         mask_size = (128, 128)
-
     else:
         raise NotImplementedError("no such model: {}".format(model_name))
 
-    train_loader, valid_loader, _ = get_data_loaders(mask_size)
+    return model, mask_size
 
+
+def main():
+    '''
+    Execute training
+    '''
+    import wandb
+    if use_wandb:
+        wandb.init(project="compvis_dtd_{}".format(model_name))
+        wandb.config.update({"Model": model_name,
+                             "Learning Rate": lr,
+                             "Batch Size": batch_size
+                             })
+    else:
+        from torch.utils.tensorboard import SummaryWriter
+        writer = SummaryWriter()
+
+    model, mask_size = get_model()
+    train_loader, valid_loader = get_data_loaders(mask_size)
     loss = MyWeightedLoss(mask_size)
-    loss.__name__ = 'bce-with-logits-loss'
 
     # loss = torch.nn.CrossEntropyLoss()
     # loss.__name__ = 'Cross Entropy Loss'
@@ -171,33 +167,10 @@ def main():
                 writer.add_scalar('{}({}/valid'.format(model_name, m_name), valid_logs[m_name], i)
 
         # Early stopping
-        if count_not_improved > 8:
+        if count_not_improved > 5:
             print("Early stopping!")
             break
 
 
-def analyze_masks():
-    train_dataset = DtdDataset('images/dtd_train', (128, 128), augmentation=None, preprocessing=None)
-
-    labels = []
-    for i in range(len(train_dataset)):
-        _, _, label = train_dataset[i]
-        labels.append(label)
-
-    labels = np.array(labels)
-    unique, counts = np.unique(labels, return_counts=True)
-    dict(zip(unique, counts))
-
-    fig = plt.figure(figsize=(8, 8))
-    width = 0.65
-    plt.bar(unique, counts, width)
-    plt.ylabel('Number of Mask')
-    plt.title('Number of Masks per Category')
-    plt.xticks(unique)
-    plt.yticks(np.arange(0, 51, 10))
-    plt.show()
-
-
 if __name__ == '__main__':
-    # analyze_masks()
     main()
